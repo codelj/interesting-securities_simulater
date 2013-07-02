f66396cd52349fdf6e34a94fc564d792e795de61
codelj <rey.ljian@yahoo.com>
2013-07-02 11:34:36 -0400

    Create Price.java

---
+++ Price.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class Price {
+  private double record;
+	private Date date;
+ 
+	Price(Date date,double record){
+		this.record = record;
+		this.date = date;		
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getRecord() {
+		return record;
+	}
+
+	public void setRecord(double record) {
+		this.record = record;
+	}
+
+}
